bbbd097f725e0c12a11ed4f399e6b8c70236f73d
Yaroslav <slasev903@gmail.com>
2021-08-05 17:21:28 +0300

    new commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const inputNum = 11;
+// document.body.innerHTML('<input type="1+1">');
+
+function plusNumber(inputNum) {
+     let plus = inputNum + 1;
+     return plus
+}
+
+console.log(plusNumber(inputNum));
+
+let string = 'less one';
+let lengsString = string.length;
+ console.log(lengsString)
+
+
+function sum(a, b) {
+     let result = a + b;
+     return result;
+}
+
+console.log(sum(110, 10));
+
+// +===================================================================================================================+\
+
+let sec = Math.random(1, 10)
+
+console.log(sec);

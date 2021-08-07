d551dfd0164d61094f3f67c7b9b8f714443110a5
Yaroslav <slasev903@gmail.com>
2021-08-07 17:37:13 +0300

    commit new

--- main.js
+++ main.js
@@ -1,43 +1,94 @@
-const inputNum = 11;
+// const inputNum = 11;
 // document.body.innerHTML('<input type="1+1">');
 
-function plusNumber(inputNum) {
-     let plus = inputNum + 1;
-     return plus
-}
+// function plusNumber(inputNum) {
+//      let plus = inputNum + 1;
+//      return plus
+// }
 
-console.log(plusNumber(inputNum));
+// console.log(plusNumber(inputNum));
 
-let string = 'less one';
-let lengsString = string.length;
- console.log(lengsString)
+// let string = 'less one';
+// let lengsString = string.length;
+//  console.log(lengsString)
 
 
-function sum(a, b) {
-     let result = a + b;
-     return result;
-}
+// function sum(a, b) {
+//      let result = a + b;
+//      return result;
+// }
 
-console.log(sum(110, 10));
+// console.log(sum(110, 10));
 
 // +===================================================================================================================+\
 
-let sec = Math.random(1, 10)
+// let sec = Math.random(1, 10)
 
-console.log(sec);
+// console.log(sec);
 
 
-var a1 = 1;
-var amp = `+`;
-var b = 4;
-let res = 'Hello'
+// var a1 = 1;
+// var amp = `+`;
+// var b = 4;
+// let res = 'Hello'
 
-function calculate(a1, b) {
-if (a >= b) {
-     var result = res;
-}
-return result;
-}
+// function calculate(a1, b) {
+// if (a >= b) {
+//      var result = res;
+// }
+// return result;
+// }
 
-console.log(calculate(a1,b)
- 
+
+//   let hello = document.write('Hello Yarik');
+
+//  var ri = document.setTimeout(document.write('Hello Yarik'), 2000);
+
+// let amount =20;
+// let cockiePrise = 7;
+// let candyPrise = 8;
+// let rest = amount - cockiePrise;
+
+// if (rest >= candyPrise) {
+//      document.write('i have enough money')
+// } else {
+//      document.write('i do not have enough money')
+// }
+
+// let amount = 90;
+// let cockiePrise = 7;
+// let candyPrise = 8;
+// amount = amount - cockiePrise;
+
+// if (amount >= candyPrise) {
+//      document.write('i have enough money')
+// } else {
+//      document.write('i do not have enough money')
+// }
+
+
+// const a = "string";
+// const aType = typeof a;
+// console.log(aType);
+
+// console.log('\xa9'); //00 - FF
+// console.log('\u23F3');//0000 - FFFF(UTF-16)
+// console.log('\u{1F514}');//0 - 10FFFF(UTF-32)
+
+const s1 = '<div class="spinner">Loading...</div>';
+
+console.log(s1);
+
+const a = 2;
+const b = 5;
+
+const s2 = `
+<h1>Title</h1>
+<ul>
+<li>${a} + ${b} = ${a + b}</li>
+<li>${a} * ${b} = ${a * b}</li>
+<li>${a} / ${b} = ${a / b}</li>
+</ul>
+`;
+
+console.log(s2);
